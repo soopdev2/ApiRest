75d2de422833e006e23341b446e55b50226d8ecc
Salvatore <Salvatore@LAPTOP-N7HQ7NL3.fritz.box>
2025-11-10 12:16:14 +0100

    corsFilter fix

--- src/main/java/Services/Filter/CorsFilter.java
+++ src/main/java/Services/Filter/CorsFilter.java
@@ -7,8 +7,6 @@ package Services.Filter;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
-import jakarta.ws.rs.container.PreMatching;
-import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.Provider;
 import java.io.IOException;
 
@@ -17,26 +15,24 @@ import java.io.IOException;
  * @author Salvatore
  */
 @Provider
-@PreMatching
-public class CorsFilter implements ContainerResponseFilter, jakarta.ws.rs.container.ContainerRequestFilter {
-
-    @Override
-    public void filter(ContainerRequestContext requestContext) throws IOException {
-        if ("OPTIONS".equalsIgnoreCase(requestContext.getMethod())) {
-            Response.ResponseBuilder builder = Response.ok();
-            builder.header("Access-Control-Allow-Origin", "https://app.findyourgoal.it");
-            builder.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-            builder.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-            builder.header("Access-Control-Allow-Credentials", "true");
-            requestContext.abortWith(builder.build());
-        }
-    }
+public class CorsFilter implements ContainerResponseFilter {
 
     @Override
     public void filter(ContainerRequestContext request, ContainerResponseContext response) throws IOException {
-        response.getHeaders().add("Access-Control-Allow-Origin", "https://app.findyourgoal.it");
-        response.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        response.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, HEAD");
-        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+
+        String origin = request.getHeaderString("Origin");
+
+        if (origin != null) {
+            response.getHeaders().putSingle("Access-Control-Allow-Origin", origin);
+            response.getHeaders().putSingle("Vary", "Origin");
+        }
+
+        response.getHeaders().putSingle("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        response.getHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        response.getHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(200);
+        }
     }
 }
